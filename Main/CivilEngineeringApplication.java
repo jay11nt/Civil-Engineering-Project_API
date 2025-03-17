ae29a51527eabcbdcf90c22fc2c60c923d3e56a5
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-17 14:18:28 +0530

    Update CivilEngineeringApplication.java

--- Main/CivilEngineeringApplication.java
+++ Main/CivilEngineeringApplication.java
@@ -1,1 +1,12 @@
+package com.example.civil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivilEngineeringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CivilEngineeringApplication.class, args);
+    }
+}
 
