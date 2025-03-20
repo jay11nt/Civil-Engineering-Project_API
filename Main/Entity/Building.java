d4eab76846fa709041208cf404844f31ff88d45a
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-20 13:57:14 +0530

    Update Building.java

--- Main/Entity/Building.java
+++ Main/Entity/Building.java
@@ -38,3 +38,9 @@ public Project getProject() {
     public void setId(Long id) {
         this.id = id;
     }
+public void setName(String name) {
+        this.name = name;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
