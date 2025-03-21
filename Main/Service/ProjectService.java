f3dbec65578670382dc1b5e95eccc2498a7e1ca5
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-21 13:39:00 +0530

    Update ProjectService.java

--- Main/Service/ProjectService.java
+++ Main/Service/ProjectService.java
@@ -1,1 +1,17 @@
+package com.example.civil.service;
 
+import com.example.civil.entity.Project;
+import com.example.civil.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
