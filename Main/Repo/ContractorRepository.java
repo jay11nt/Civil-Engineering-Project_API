905bd25031626be8627829c9b4684a86b3de7b19
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-21 13:15:47 +0530

    Update ContractorRepository.java

--- Main/Repo/ContractorRepository.java
+++ Main/Repo/ContractorRepository.java
@@ -3,3 +3,7 @@ package com.example.civil.repository;
 import com.example.civil.entity.Contractor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContractorRepository extends JpaRepository<Contractor, Long> {
+}
