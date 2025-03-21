16d582d347bd448148bb13c5ff10cdb8722609ba
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-21 13:44:19 +0530

    Update BuildingService.java

--- Main/Service/BuildingService.java
+++ Main/Service/BuildingService.java
@@ -1,1 +1,8 @@
+package com.example.civil.service;
 
+import com.example.civil.entity.Building;
+import com.example.civil.repository.BuildingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
