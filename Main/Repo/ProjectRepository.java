3ea7a7ad14dbc0f6aa1f934bf35b3b1d6df054db
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-21 13:16:53 +0530

    Update ProjectRepository.java

--- Main/Repo/ProjectRepository.java
+++ Main/Repo/ProjectRepository.java
@@ -3,3 +3,8 @@ package com.example.civil.repository;
 import com.example.civil.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
