4d85b55fa0346c0dae9a810b2112963e6a35e70c
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-21 13:20:29 +0530

    Update BuildingRepository.java

--- Main/Repo/BuildingRepository.java
+++ Main/Repo/BuildingRepository.java
@@ -1,1 +1,9 @@
+package com.example.civil.repository;
 
+import com.example.civil.entity.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+}
