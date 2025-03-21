5bfac744022fca973c729b8ab2faba0225e033ad
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-21 13:16:36 +0530

    Update ProjectRepository.java

--- Main/Repo/ProjectRepository.java
+++ Main/Repo/ProjectRepository.java
@@ -1,1 +1,5 @@
+package com.example.civil.repository;
 
+import com.example.civil.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
