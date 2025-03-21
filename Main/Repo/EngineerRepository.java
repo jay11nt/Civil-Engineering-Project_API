d95d7288a890a89a36f2f0847080b0e9bd7edd8a
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-21 13:18:11 +0530

    Update EngineerRepository.java

--- Main/Repo/EngineerRepository.java
+++ Main/Repo/EngineerRepository.java
@@ -1,1 +1,9 @@
+package com.example.civil.repository;
 
+import com.example.civil.entity.Engineer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EngineerRepository extends JpaRepository<Engineer, Long> {
+}
