c702af99eff1a01a05217ec4c3053275afc969ee
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-21 13:15:32 +0530

    Update ContractorRepository.java

--- Main/Repo/ContractorRepository.java
+++ Main/Repo/ContractorRepository.java
@@ -2,3 +2,4 @@ package com.example.civil.repository;
 
 import com.example.civil.entity.Contractor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
