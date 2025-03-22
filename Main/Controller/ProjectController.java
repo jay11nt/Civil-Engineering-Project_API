b950940e076e2edfe4b868bb9204d69b6e11c7e8
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-22 13:43:47 +0530

    Update ProjectController.java

--- Main/Controller/ProjectController.java
+++ Main/Controller/ProjectController.java
@@ -6,3 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+    private final ProjectService projectService;
